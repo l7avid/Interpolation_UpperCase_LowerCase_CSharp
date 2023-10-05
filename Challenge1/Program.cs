1719ebdac493c25297edac3341e68912bedab2fc
David Reina <davidereina@hotmail.com>
2023-10-05 14:55:29 -0500

    Using interpolation to show in the console the name entered by the user with other text, and an example of how to use Substring() method

---
+++ Challenge1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Challenge1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.Write("Please enter your name and press enter: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Name in upper case: {0}", name.ToUpper());
+            Console.WriteLine($"Name in lower case: {name.ToLower()}");
+            Console.WriteLine("Name substring 3: " + name.Substring(3));
+        }
+    }
+}
